c59d893f70a501fecb70d2d71fc88d5630f2e6d8
asakurasol <mamamia1734@gmail.com>
2015-04-06 19:50:14 -0700

    throw error given null or undefined directory

--- server-typescript/server/processAPIs/fs.ts
+++ server-typescript/server/processAPIs/fs.ts
@@ -27,8 +27,11 @@ function fsWrapper(fsCallback, args){
     return function(socket){
         return function(opts){
 
-            if(!opts.dir) opts.dir = '/';
-            console.log('options are ', opts);
+            // if(!opts.dir) opts.dir = '/';
+            if(!opts.dir){
+                socket.emit(opts.uid, utility.wrapperResponse({ errno: 99, code: 'CUSTOM', desc: 'No directory given' }, null));
+            }
+
             var arguments = args.map(function(arg){
                 return opts[arg];
             });
