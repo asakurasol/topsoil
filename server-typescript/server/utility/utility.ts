10875c570e8a4a170b9cf52167d80d6aaf478e2e
asakurasol <mamamia1734@gmail.com>
2015-04-13 20:01:59 -0700

    add git diff to view

--- server-typescript/server/utility/utility.ts
+++ server-typescript/server/utility/utility.ts
@@ -102,6 +102,15 @@ utility.identity = function(data){
         return '';
     }
     return data;
-}
+};
+
+utility.logIdentity = function(data){
+    if(!data){
+        console.log('no data passed');
+        return '';
+    }
+    console.log(data);
+    return data;
+};
 
 module.exports = utility;
